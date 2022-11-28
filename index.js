fdd8713f6f6c5818df4d9eb75bd412f1ab93d7f3
Brian <wamwangimathenge@gmail.com>
2022-11-28 15:39:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// const cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(cards){
+    let message = []
+    for(const name of cards) {
+        message.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+        debugger;
+    }
+    return message;
+};
+
+function countDown(num) {
+    for(let i=num; i >= 0; i--) {
+        console.log(i);
+    }
+}
